5cd04e7c956efb6c2beb8e4d342934c4e2a334b2
lhjtrades <lhjtrades@gmail.com>
2020-08-07 22:13:02 +0000

    first commit

--- app.py
+++ app.py
@@ -3,25 +3,24 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-# from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
+from datetime import datetime
+import os
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+app.config['SECRET'] = os.getenv("SECRET_KEY")
+SECRET = app.config['SECRET']
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
+app.config['MONGO_DBNAME'] = 'users'
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
+app.config['MONGO_URI'] = f'mongodb+srv://admin_2:{SECRET}@cluster0.fnifx.mongodb.net/users?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 # -- Routes section --
 # INDEX
@@ -30,17 +29,4 @@ events = [
 @app.route('/index')
 
 def index():
-    return render_template('index.html', events = events)
-
-
-# CONNECT TO DB, ADD DATA
-
-@app.route('/add')
-
-def add():
-    # connect to the database
-
-    # insert new data
-
-    # return a message to the user
-    return ""
+    return render_template('index.html', time = datetime.now())
